1b501f25aa042c7cec109dbdbeeadd0b9f3edde5
hsootree <hsootree@gmail.com>
2017-08-06 21:47:12 +0900

    test function

---
+++ lecture_test/javascript/function.js
@@ -0,0 +1,17 @@
+
+/*
+function sum() {
+  var res = 0;
+  for (var i=0; i<arguments.length; i++) {
+    res += arguments[i];
+  }
+  return res;
+}
+console.log(sum());
+console.log(sum(1,2));
+console.log(sum(1,2,3));
+*/
+
+if (var i = 0, i >20, i++){
+  
+}
