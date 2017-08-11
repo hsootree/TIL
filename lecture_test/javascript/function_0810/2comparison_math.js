ebe02586a5094301a50e068a6b9eab56b967f474
hsootree <hsootree@gmail.com>
2017-08-11 22:34:57 +0900

    add computer network study file

---
+++ lecture_test/javascript/function_0810/2comparison_math.js
@@ -0,0 +1,12 @@
+// 1. 두 수를 입력받아 큰 수를 반환하는 함수
+
+
+// Math.max() Math 객체를 이용
+/*
+function max() {
+  var result;
+  Math.max(a, b);
+  return result;
+} */
+console.log(Math.max(5, 9)); // 빌트인 함수 이용. 완료.
+
