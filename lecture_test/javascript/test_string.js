49a0d1040175ee8562abac8a080ca00472d341df
hsootree <hsootree@gmail.com>
2017-08-13 21:37:56 +0900

    add more prototype

--- lecture_test/javascript/test_string.js
+++ lecture_test/javascript/test_string.js
@@ -1,10 +1,12 @@
 // 기본 자료형 string을 위한 레퍼(wrapper) 객체
 
-var str = 'Hello world!'; // 할당한다.
-var res = str.toUpperCase(); // 대문자로 전환하여 할당한다.
+  var str = 'Hello world!'; // 할당한다.
+  var res = str.toUpperCase(); // 대문자로 전환하여 할당한다.
+  console.log(res); // 출력 : HELLO WORLD -done!
 
-console.log(res); // 출력 : HELLO WORLD -done!
-
+// 3.7 String.prototype.toLowerCase()
+  var res = str.toLowerCase(); // 문자열의 문자를 모두 소문자로 전환하여 할당한다.
+  console.log(res); // hello world!
 
 // 1. String Constructor
 // String 객체는 String() 생성자 함수를 통해 생성할 수 있다. 이때 전달된 인자는 모두 문자열로 변환된다.
@@ -51,6 +53,7 @@ console.log(res); // 출력 : HELLO WORLD -done!
     console.log(str.charAt(i)); // i에 해당하는 index의 값에 위치한 문자를 출력한다. 기대값 : 안, 녕, 하, 세, 요 이 순서대로 개행을 하면서 출력됨. 
   }
 
+// String.prototype.indexOf()
 // str.indexOf(searchValue[, fromIndex])
 // searchValue: 검색할 문자 또는 문자열
 // fromIndex : 검색 시작 index (생략할 경우, 0)
@@ -58,8 +61,12 @@ var str = 'Hollow World';
   console.log(str.indexOf('l')); // l 문자를 검색 fromindex를 생략했으므로 0부터 시작함. 카운트는 어떻게 하지? 개수를 어떻게 구하지?
   // for문을 사용하여 반복해서 찾고 출력이 되는 값을 더하면 갯수를 구할 수 있을까?
   // 결과 : l 이 총 3개이므로 3 결과값이 출력되면 됨.
-  
-// String.prototype.replace()
+
+// String.prototype.lastIndexOf()
+
+
+
+// 3.4 String.prototype.replace()
 // 첫번째 인수로 전달된 문자열 또는 정규표현식을 대상 문자열에서 검색하여 두번째 인수로 전달된문자열로 대체한다. 검색된 문자열이 복수 존재할 경우 첫번째로 검색된 문자열만 대체된다
 // str.replace(pattern, replacement[, flags])
 // pattern: 검색 대상 문자열 또는 정규표현식
@@ -70,4 +77,39 @@ var str = 'Hollow World';
   console.log(replacedStr); // 치환한 변수를 출력한다.
   console.log(str); // 치환전의 변수를 출력한다.
 
-  
+  replacedStr = str.replace(/l/gi, 'soo'); // 정규표현식
+  // gi는 flag라 하고 g(global)는 문자열 내에 패턴과 일치하는 모든 문자열, i(ignore)는 대소문자를 구분하지 않는다.
+  console.log(replacedStr); // Hesoosooo Worsood 치환한 변수를 출력한다.
+  console.log(str); // 치환전의 변수를 출력한다. 치환은 변수에 할당된 값을 바꾸지는 않는다.
+
+// 3.5 tring.prototype.split()
+// str.split([separator[, limit]])
+// separator: 구분 대상 문자열 또는 정규표현식
+// limit: 구분 대상수의 한계를 나타내는 정수
+  var str = 'How are you doing?';
+  var splitStr = str.split(' '); // 공백을 기준으로 구분한다.
+  console.log(splitStr); // 구분하한 것을 출력한다. ["How", "are", "you", "doing?"]
+
+  splitStr = str.split(); // 구분을 지정하지 않고 구분을 할당하면
+  console.log(splitStr); // [ 'How are you doing' ] 구분하지 않는 하나의 배열로 표현된다.
+
+  splitStr = str.split(''); // 최소단위로 구분한다.
+  console.log(splitStr); // 출력 : ["H", "o", "w", " ", "a", "r", "e", " ", "y", "o", "u", " ", "d", "o", "i", "n", "g", "?"]
+
+  splitStr = str.split(' ', 2); // 공백을 기준으로 구분하고 2번째 문자열까지만
+  console.log(splitStr); // 출력 : ["How", "are"] 2번째 까지 출력된다.
+
+  splitStr = str.split('o'); // 문자 o를 기준으로 구분한다.
+  console.log(splitStr); // 출력 : [ "H", "w are y", "u d", "ing?"]
+
+// 3.6 String.prototype.substring()
+// str.substring(indexA[, indexB])
+// indexA: 0 ~ 해당문자열 길이 -1 까지의 정수
+// indexB: 0 ~ 해당문자열 길이까지의 정수
+
+// 3.9 String.prototype.trim()
+  var str = '   foo  '; // 문자열 앞뒤로 공백이 있는 문자를 할당한다.
+  var trimmedStr = str.trim(); // 문자열 양쪽 끝의 공백 문자를 제거한 것을 할당한다.
+  console.log(trimmedStr); // 공백을 제거한 것을 출력한다.
+  console.log(str); // 앞뒤로 공백이 있는 문자열을 할당한 것을 출력한다.
+
